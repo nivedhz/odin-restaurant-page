19bdf7d5f486bdb4b69e374eaf4a7813ecc61334
Nivedh E R <nivedher12@gmail.com>
2026-04-27 20:39:20 +0530

    feat(home): added the layout and styled the home page

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import "./styles/style.css";
-// import { createAboutPage } from "./pages/about.js";
-// import { createHomePage } from "./pages/home.js";
-// import { createMenuPage } from "./pages/menu.js";
-//
-// const DOM = {
-//   button: {},
-//   container: {
-//     navBtn: document.querySelector(".main-header__nav"),
-//     contentContainer: document.querySelector("#content"),
-//   },
-// };
-// const state = {
-//   nav: [createHomePage, createMenuPage, createAboutPage],
-// };
-//
-// function showDefault() {
-//   DOM.container.contentContainer.replaceChildren(createHomePage());
-// }
-//
-// function handleSwitching(event) {
-//   const index = Number(event.target.id);
-//   DOM.container.contentContainer.replaceChildren(state.nav[index]());
-// }
-//
-// showDefault();
-// DOM.container.navBtn.addEventListener("click", handleSwitching);
+import "./styles/home.css";
+import { createAboutPage } from "./pages/about.js";
+import { createHomePage } from "./pages/home.js";
+import { createMenuPage } from "./pages/menu.js";
+
+const DOM = {
+  button: {},
+  container: {
+    navBtn: document.querySelector(".main-header__nav"),
+    contentContainer: document.querySelector("#content"),
+  },
+};
+const state = {
+  nav: [createHomePage, createMenuPage, createAboutPage],
+};
+
+function showDefault() {
+  DOM.container.contentContainer.replaceChildren(createHomePage());
+}
+
+function handleSwitching(event) {
+  const index = Number(event.target.id);
+  DOM.container.contentContainer.replaceChildren(state.nav[index]());
+}
+
+showDefault();
+DOM.container.navBtn.addEventListener("click", handleSwitching);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,56 @@
+import restoImg from "../assets/images/resto.jpg";
+import { createMenuPage } from "./menu.js";
+
+const state = {
+  gridMsg: [
+    "Elevating dining into an art form.",
+    "Every detail thoughtfully curated",
+    "Indulge in timeless taste",
+    "Not just a meal but a moment",
+    "The kind of place you don't forget",
+  ],
+};
+
+function renderMenu() {
+  document.querySelector("#content").replaceChildren(createMenuPage());
+}
 function createHomePage() {
-  let heading = document.createElement("h1");
-  heading.textContent = "This is the home";
-  return heading;
+  const homeContainer = document.createElement("main");
+  homeContainer.classList.add("home-container");
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+
+  const restoImgEl = document.createElement("img");
+  restoImgEl.classList.add("resto-img");
+  restoImgEl.src = restoImg;
+
+  const welcomeMsgContainer = document.createElement("div");
+  welcomeMsgContainer.classList.add("welcome-msg-container");
+  const restoName = document.createElement("h1");
+  restoName.textContent = "Dining Palace";
+  const welcomeMsg = document.createElement("p");
+  welcomeMsg.textContent = "Welcome to our restaurant...";
+  const menuBtn = document.createElement("button");
+  menuBtn.classList.add("menu-btn");
+  menuBtn.textContent = "Go to Menu";
+  menuBtn.addEventListener("click", renderMenu);
+
+  const gridContainer = document.createElement("div");
+  gridContainer.classList.add("grid-container");
+  state.gridMsg.forEach((msg) => {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("message-grid");
+    gridItem.textContent = msg;
+    gridContainer.appendChild(gridItem);
+  });
+
+  welcomeMsgContainer.append(restoName, welcomeMsg, menuBtn);
+  imgContainer.appendChild(welcomeMsgContainer);
+  imgContainer.appendChild(restoImgEl);
+  homeContainer.appendChild(imgContainer);
+  homeContainer.appendChild(gridContainer);
+
+  return homeContainer;
 }
 
 export { createHomePage };
